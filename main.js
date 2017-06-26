5e0a2f8b82cfb8c236a71e9b0ce0b1632e05be89
RiceAndMeet <jun.ying.yan@ryerson.ca>
2017-06-26 17:31:16 -0400

    layout changes

--- main.js
+++ main.js
@@ -1,34 +1,36 @@
-//jquery animation when scrolling 
+//jquery animation when scrolling ------------------------------
 $.fn.scrollView = function () {
   return this.each(function () {
     $('html, body').animate({
       //destination position accounting for size of navbar 
-      scrollTop: $(this).offset().top//-$window.height() 
+      scrollTop: $(this).offset().top 
     }, 1000);
   });
 }
+//preloading images -------------------------------------------------
+$.fn.preload = function() {
+    this.each(function(){
+        (new Image()).src = this;
+    });
+}
+var images =['images/Steven.jpg','images/soccer.jpg',
+'images/Toronto.jpg','images/beginner.jpg','images/level1.png'];
 
 window.onload = function(){
-  $('#hello').scrollView();  
+  $('#page0').scrollView();  
+  $(images).preload();
   $('#loader').css('top','-100vh');
   $('#loader').css ('animation-play-state','paused');
 }
 
-var mapNavbarToPage = function (nav_bar_id){
-  if (nav_bar_id == "intro") return "hello";
-  else if (nav_bar_id == "bio") return "aboutMe";
-  else if (nav_bar_id == "portfolio") return "projects";
-  else if (nav_bar_id == "contactMe") return "contact";
-}
-
-//scroll position detection 
+//scroll position detection ---------------------------------------------------------------
 var $animation_elements = $('.animation-element'); 
 var $window = $(window);
 //check if the specified element is in view 
 function check_if_in_view() {
   // scale down window height to 91%, accounting for the height of tool bar   
-  var window_height =Math.trunc($window.height() *0.91);
-  var window_top_position = Math.trunc($window.scrollTop()+$window.height()*0.09);
+  var window_height =Math.trunc($window.height());
+  var window_top_position = Math.trunc($window.scrollTop()+$window.height());
   var window_bottom_position = (window_top_position + window_height);
   var current_scroll_direction = $(this).scrollTop();
   //change animation-element's astatus base on its position 
@@ -45,14 +47,12 @@ function check_if_in_view() {
     } else {
       $element.removeClass('in-view');
     }
-    console.log(element_top_position);
   });
-  console.log('window'+window_bottom_position);
 }
 $window.on('scroll resize', check_if_in_view);
 $window.trigger('scroll');
 
-//creating footer button dynamicially
+//creating footer button dynamicially------------------------------------------------------
 $elements = $('.page-footer');
 var $button = $('<button>',{'class':'btn btn-secondary hvr-bob','id':'footerButton'});
 $button[0].innerHTML= 'About Me <span class="glyphicon glyphicon-download"></span>';
@@ -61,151 +61,127 @@ $button.click(function(event){
     var current_nav_item = $('.active')[0];
     var element = $(this)[0];
     //match nav-items with corresponding page
-    if (current_nav_item.id.indexOf("intro")!== -1){
+    if (current_nav_item.id.indexOf("nav0")!== -1){
       element.innerHTML = 'My Projects <span class="glyphicon glyphicon-download"></span>';
-      $("#aboutMe").scrollView();
-      $('#intro').removeClass('active');
-      $("#bio").addClass('active');
+      $("#page1").scrollView();
+      $('#nav0').removeClass('active');
+      $("#nav1").addClass('active');
     }
-    else if (current_nav_item.id.indexOf("bio")!== -1){
+    else if (current_nav_item.id.indexOf("nav1")!== -1){
       element.innerHTML = 'Contact Me <span class="glyphicon glyphicon-download"></span>';
-      $("#projects").scrollView();
-      $('#bio').removeClass('active');
-      $('#portfolio').addClass('active');
+      $("#page2").scrollView();
+      $('#nav1').removeClass('active');
+      $('#nav2').addClass('active');
     }
-    else if (current_nav_item.id.indexOf("portfolio") !== -1){
+    else if (current_nav_item.id.indexOf("nav2") !== -1){
       $('#footerButton').hide();
-      $("#contact").scrollView();
-      $('#portfolio').removeClass('active');
-      $('#contactMe').addClass('active');
+      $("#page3").scrollView();
+      $('#nav2').removeClass('active');
+      $('#nav3').addClass('active');
     }
 });
 $elements.append($button);
 
-// for mobile versison, expanding navbar 
-$('#navbarLinks').on('click',function(event){
-  $('#navbarLinks').toggleClass('responsive');
-});
-var $nav_elements = $('#navbarLinks')[0].children;
+
 //add actionlistener to nav-items, when click it will scroll to
-//corresponding destination on the DOM 
+//corresponding destination on the DOM ----------------------------------------------------------------------
+var $nav_elements = $('#navbarLinks')[0].children;
 $.each($nav_elements,function(){
    var element = $(this).attr('id');
    var button_element = $button[0];
-   if (element == "intro"){
-      $('#intro').click(function(event){
-          $('#hello').scrollView();
-          $(".active")[0].className="nav-item hvr-float-shadow";
-          $('#intro')[0].className+= ' active';
-          button_element.innerHTML= 
-            'About Me <span class="glyphicon glyphicon-download"></span>';
+   var $selector = element.replace('nav','page'); 
+   $(this).click(function(){
+      $("#"+$selector).scrollView();
+      selectPage(element);
+      $(".active").removeClass('active');
+      $(this).addClass('active');
+      if(element == "nav0"){
+           button_element.innerHTML= 
+          'About Me <span class="glyphicon glyphicon-download"></span>';
           $('#footerButton').show();
-      });
-   }
-   else if (element == "bio"){
-      $('#bio').click(function(event){
-          $('#aboutMe').scrollView();
-          $(".active")[0].className="nav-item hvr-float-shadow";
-          $('#bio')[0].className+= ' active';
-          button_element.innerHTML= 
-            'My Projects <span class="glyphicon glyphicon-download"></span>';
+       }
+       else if(element == "nav1"){
+         button_element.innerHTML= 
+        'My Projects <span class="glyphicon glyphicon-download"></span>';
           $('#footerButton').show();
-      });
-   }
-   else if (element == "portfolio"){
-      $('#portfolio').click(function(event){
-          $('#projects').scrollView();
-          $(".active")[0].className="nav-item hvr-float-shadow";
-          $('#portfolio')[0].className+= ' active';
+       }
+       else if(element == "nav2"){
           button_element.innerHTML= 
-            'Contact Me <span class="glyphicon glyphicon-download"></span>';
-          $('#footerButton').show();
-      });
-   }
-   else if (element == "contactMe"){
-      $('#contactMe').click(function(event){
-          $('#contact').scrollView();
-          $(".active")[0].className="nav-item hvr-float-shadow";
-          $('#contactMe')[0].className+= ' active';
+          'Contact Me <span class="glyphicon glyphicon-download"></span>';
+            $('#footerButton').show();
+       }
+       else if(element == "nav3"){
           $('#footerButton').hide();
-      });
-   }
+       }
+   });
 });
-
-// when hovering keywords on the intro key, display background image corresponding to that keyword
-$('#steven').hover(
-  function(event){
-    $('#hello').css("background-image","url(images/Steven.jpg)");
-    $('#hello').css("background-size","100% 100%");
-    $(this).css("cursor","pointer");
-    $('#hello h1,#hello h2,.underline_keywords').css("color","transparent");//make other text disappear
-    $(this).css("color","white");
-    $('#intro_text .counter').css("display",'none'); //make the animated counter disappear
-  }, function(event){// revert the changes 
-    $('#hello').css("background-image",'');
-    $('#hello').css("background-size",'cover');
-    $('#intro_text h1,#intro_text h2,.underline_keywords').css("color","rgb(77, 77, 77)");
-    $('.underline_keywords').css("color","rgb(67, 125, 219)");
-    $('#intro_text .counter').css("display",'inline-block');
+//sliding content-pages 
+function selectPage (selector){
+  $selector = "#" + selector;
+  $contentSelector = $selector.replace("nav", "page");
+  
+  if ($($contentSelector).hasClass("right-page")){
+    $(".center-page").removeClass("center-page").addClass("left-page");
+    $($contentSelector).removeClass("right-page");
+    $($contentSelector).addClass("center-page");
   }
-);
-$('#soccer').hover(
-  function(event){
-    $('#hello').css("background-image","url(images/soccer.jpg)");
-    $(this).css("cursor","pointer");
-    $('#hello h1,#hello h2,.underline_keywords').css("color","transparent");
-    $(this).css("color","black");
-    $('#intro_text .counter').css("display",'none'); //make the animated counter disappear
-  }, function(event){// revert the changes 
-    $('#hello').css("background-image",'');
-    $('#intro_text h1,#intro_text h2,.underline_keywords').css("color","rgb(77, 77, 77)");
-    $('.underline_keywords').css("color","rgb(67, 125, 219)");
-    $('#intro_text .counter').css("display",'inline-block');
-  }
-);
-$('#toronto').hover(
-  function(event){
-    $('#hello').css("background-image","url(http://imgprix.com/web/wallpapers/toronto-skyline-ontario-canada/1920x1080.jpg)");
-    $(this).css("cursor","pointer");
-    $('#intro_text h1,#intro_text h2,.underline_keywords').css("color","transparent");
-    $(this).css("color","white");
-    $('#intro_text .counter').css("display",'none'); //make the animated counter disappear
-  },function(event){
-    $('#hello').css("background-image",'');
-    $('#intro_text h1,#intro_text h2,.underline_keywords').css("color","rgb(77, 77, 77)");
-    $('.underline_keywords').css("color","rgb(67, 125, 219)");
-    $('#intro_text .counter').css("display",'inline-block');
-  }
-)
+  else if ($($contentSelector).hasClass("left-page")) {
+    $(".center-page").removeClass("center-page").addClass("right-page");
+    $($contentSelector).addClass("center-page");
+    $($contentSelector).removeClass("left-page");
+  }  
+}
 
+// when hovering keywords on the intro key, display background image corresponding to that keyword----------------------
+$.fn.appear_on_hover = function(image){
+  $(this).hover(
+   function(event){
+      $('#page0').css({"background-image":"url("+image+")","background-size":"100% 100%"});
+      $('#page0 h1,#page0 h2,.underline_keywords').css("color","transparent");//make other text disappear
+      $('.underline_keywords').css('display','none');
+      $(this).css({"cursor":"pointer","color":"white",'display':'inline-block'});
+      $('#intro_text .counter').css("display",'none'); //make the animated counter disappear
+      $('#intro_text .content').css("display","none");
+    }, function(event){// revert the changes 
+      $('#page0').css({"background-image":'',"background-size":'cover'});
+      $('#intro_text h1,#intro_text h2,.underline_keywords').css("color","rgb(77, 77, 77)");
+      $('.underline_keywords').css({"color":"rgb(67, 125, 219)","display":"inline-block"});
+      $('#intro_text .counter').css("display",'inline-block');
+      $('#intro_text .content').css("display","block");
+    });
+}
+$('#steven').appear_on_hover('images/Steven.jpg');
+$('#soccer').appear_on_hover("images/soccer.jpg");
+$('#toronto').appear_on_hover("images/Toronto.jpg");
 
+//-----------------------------------------------------------------------
 $(".circle").click(function() {
-  var spanNum = $(this).attr("id");
-  selectCircle(spanNum);
+  var page = $(this).attr("id");
+  selectCircle(page);
 });
-
+// sliding page for about me 
 function selectCircle(selector) {
   //match corresponding page associated with the circle 
   $selector = "#" + selector;
-  $spanSelector = $selector.replace("circle", "content");
-  var current = $selector.replace("circle", "");
+  $contentSelector = $selector.replace("circle", "content");
   
   $(".active_timeline").removeClass("active_timeline");
-  $($selector).addClass("active_timeline");
+  $($selector+' .ch-item').addClass("active_timeline");
+  $($selector+' .mobile_nav_circle').addClass("active_timeline");
   
   //page slider for left and right 
-  if ($($spanSelector).hasClass("right")) {
-    $(".center").removeClass("center").addClass("left")
-    $($spanSelector).addClass("center");
-    $($spanSelector).removeClass("right")
-  } else if ($($spanSelector).hasClass("left")) {
-    $(".center").removeClass("center").addClass("right");
-    $($spanSelector).addClass("center");
-    $($spanSelector).removeClass("left");
+  if ($($contentSelector).hasClass("right")) {
+    $(".page_slide.center").removeClass("center").addClass("left")
+    $($contentSelector).addClass("center");
+    $($contentSelector).removeClass("right");
+  } else if ($($contentSelector).hasClass("left")) {
+    $(".page_slide.center").removeClass("center").addClass("right");
+    $($contentSelector).addClass("center");
+    $($contentSelector).removeClass("left");
   }; 
 };
 
-//animation for changing text in the HELLO Page
+//animation for changing text in the HELLO Page------------------------------
 var container = ['Java EE', "C/C++", "Web"];
 var index=0;
 var int = setInterval(calcValues, 1000);
@@ -231,7 +207,7 @@ function calcValues() {
 }
 calcValues();
 
-//animation for flip card 
+//animation for flip card -----------------------------------------------------------------
 $(document).ready(function(){
     // set up hover panels
     // although this can be done without JavaScript, we've attached these events
@@ -243,3 +219,10 @@ $(document).ready(function(){
     });
   });
 
+//---------------------------
+
+$.each($('.tl-item'),function(){
+  $(this).dblclick(function(){
+    $('#nav2').trigger('click');
+  });
+});
